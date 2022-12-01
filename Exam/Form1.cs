189381c5bfb57941985b3819643c5fef8bb23499
Chaymae Mechouahi <113052518+ChaymaeMechouahi@users.noreply.github.com>
2022-12-01 22:45:05 +0100

    Update Form1.cs

--- Exam/Form1.cs
+++ Exam/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,11 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
 
 namespace Exam
 {
     public partial class Form1 : Form
     {
+        string boxmat = "";
+        string boxdat = "";
+        string boxdure = "";
+        string note = "";
+        private static string dataconnection = @"DESKTOP-72A5J1H\SQLEXPRESS";
+        private static string database = "Examen";
+        private static string user = "root";
+        private static string password = "";
+        string connString = @"Data Source=" + dataconnection + ";Initial Catalog="
+                        + database + ";Persist Security Info=True;User ID=" + user + ";Password=" + password;
+        string querystring = "";
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +33,59 @@ namespace Exam
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            box_date.Text = "";
+            box_durée.Text = "";
+            box_matiere.Text = "";
+            box_note.Text = "";
 
         }
+        private void box_matiere_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          boxmat = box_matiere.Text  ;
+        }
+        private void box_date_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            boxdat = box_date.Text;
+
+        }
+        private void box_durée_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            boxdure = box_durée.Text;
+
+        }
+        private void box_note_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            note = box_note.Text;
+
+        }
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connString);
+            querystring = "INSERT INTO Exam VALUES (boxmat,boxdat,boxdure,note) ";
+            SqlCommand save = new SqlCommand(querystring, conn);
+            try
+            {
+                MessageBox.Show("Openning Connection ...");
+
+                //open connection
+                conn.Open();
+
+                MessageBox.Show("Connection successful!");
+                save.ExecuteNonQuery();
+             }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Error: " + exp.Message);
+            }
+            finally
+            {
+                //close connection 
+                conn.Close();
+            }
+
+            
+        }
+
+
     }
 }
